98d528a92307d6fe15d03bccc6c9c22387134154
OFFICIALSEBAS <sebastian.bonillac@udea.edu.co>
2024-06-16 17:00:14 -0500

    Listas dinamicas de tareas

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Doblar ropa', completed: false},
+  {text: 'Gym', completed: true},
+  {text: 'Futbol', completed: false},
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Sebas !!!
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
 
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text = {todo.text}
+          completed = {todo.completed}
+          />
+        ))}
+      </TodoList>
 
-function TodoItem(){
-  return (
-    <li>
-      <span>V</span>
-      <p>Llorar con la llorona</p>
-      <span>X</span>
-    </li>
+      <CreateTodoButton />
+
+    </React.Fragment>
   );
 }
 
