7d58312979fdeb046399ea51d4b4292a86857ec1
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-07-06 09:23:01 -0500

    Implementados custon hooks

--- src/App.js
+++ src/App.js
@@ -7,16 +7,36 @@ import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
 
-const defaultTodos = [
-  {text: 'Cortar cebolla', completed: true},
-  {text: 'Doblar ropa', completed: false},
-  {text: 'Gym', completed: true},
-  {text: 'Futbol', completed: false},
-]
+// const defaultTodos = [
+//   {text: 'Cortar cebolla', completed: true},
+//   {text: 'Doblar ropa', completed: false},
+//   {text: 'Gym', completed: true},
+//   {text: 'Futbol', completed: false},
+// ]
 
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1');
+
+
+function useLocalStorage(){
+
+  const [item,setItem] = React.useState()
+
+  const saveItem = (newTodos)=>{
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setItem(newTodos);
+  }
+}
 
 function App() {
-  const [todos, setTodos]= React.useState(defaultTodos);
+
+
+  const [todos, setTodos] = React.useState(() => {
+    const todosFromStorage = localStorage.getItem('TODOS_V1')
+    if (todosFromStorage) return JSON.parse(todosFromStorage)
+    return []
+  })
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo)=> todo.completed).length;
@@ -30,14 +50,14 @@ function App() {
     const newTodos = [...todos];
     const indexTodo = newTodos.findIndex( element => element.text === text);
     newTodos[indexTodo].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text)=>{
     const newTodos = [...todos]
     const indexTodo = todos.findIndex((element)=> element.text === text);
     newTodos.splice(indexTodo, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
